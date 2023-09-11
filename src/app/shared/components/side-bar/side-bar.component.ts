fadabc7cbd2326fb7b380ea560d5d19d281b1215
Paul Llanque <llanquepol@gmail.com>
2023-09-10 21:21:41 -0500

    [ADD] app-side-bar component

--- src/app/shared/components/side-bar/side-bar.component.ts
+++ src/app/shared/components/side-bar/side-bar.component.ts
@@ -3,8 +3,62 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
-  styleUrls: ['./side-bar.component.css']
+  styleUrls: ['./side-bar.component.css'],
 })
 export class SideBarComponent {
+  mainMenu: {
+    defaultOptions: any[];
+    accessLink: any[];
+  } = {
+    defaultOptions: [],
+    accessLink: [],
+  };
 
+  customOptions: any[] = [];
+
+  ngOnInit() {
+    this.mainMenu.defaultOptions = [
+      {
+        name: 'Home',
+        icon: 'uil uil-estate',
+        router: ['/'],
+      },
+      {
+        name: 'Search',
+        icon: 'uil uil-search',
+        router: ['/search'],
+      },
+      {
+        name: 'Your Library',
+        icon: 'uil uil-music',
+        router: ['/library'],
+      },
+    ];
+
+    this.mainMenu.accessLink = [
+      {
+        name: 'Create Playlist',
+        icon: 'uil uil-plus',
+      },
+      {
+        name: 'Favorites',
+        icon: 'uil uil-heart',
+      },
+    ];
+
+    this.customOptions = [
+      {
+        name: 'My 1° song',
+        router: ['/'],
+      },
+      {
+        name: 'My 2° song',
+        router: ['/'],
+      },
+      {
+        name: 'My 3° song',
+        router: ['/'],
+      },
+    ];
+  }
 }
