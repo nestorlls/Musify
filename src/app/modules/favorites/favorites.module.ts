1109ff600aff943d2bdc644a0ce32fa97f017b05
Paul Llanque <llanquepol@gmail.com>
2023-09-13 13:12:15 -0500

    [ADD] FavoritePage create and added in routing

--- src/app/modules/favorites/favorites.module.ts
+++ src/app/modules/favorites/favorites.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FavoritesRoutingModule } from './favorites-routing.module';
+import { FavoritePageComponent } from './pages/favorite-page/favorite-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FavoritePageComponent
+  ],
   imports: [
     CommonModule,
     FavoritesRoutingModule
