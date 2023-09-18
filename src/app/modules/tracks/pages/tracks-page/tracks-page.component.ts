e73bf2c0da9de6809362432266f0f653280553a2
Paul Llanque <llanquepol@gmail.com>
2023-09-18 14:29:57 -0500

    [ADD] data tracks - added

--- src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
+++ src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import * as dataRaw from '../../../../data/tracks.json';
+import { TrackModel } from '@core/models/tracks.model';
 
 @Component({
   selector: 'app-tracks-page',
   templateUrl: './tracks-page.component.html',
-  styleUrls: ['./tracks-page.component.css']
+  styleUrls: ['./tracks-page.component.css'],
 })
 export class TracksPageComponent {
+  dataTracks: TrackModel[] = [];
 
+  constructor() {}
+
+  ngOnInit(): void {
+    const { data }: any = (dataRaw as any).default;
+    this.dataTracks = data;
+  }
 }
