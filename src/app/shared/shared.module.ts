f5a2c5c5857ce2de65c8a277f645d7fdc6240ea2
Paul Llanque <llanquepol@gmail.com>
2023-09-18 14:44:53 -0500

    [ADD] list header and body components added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,6 +5,8 @@ import { MediaPlayerComponent } from './components/media-player/media-player.com
 import { HeaderUserComponent } from './components/header-user/header-user.component';
 import { CardPlayerComponent } from './components/card-player/card-player.component';
 import { SectionGenericComponent } from './components/section-generic/section-generic.component';
+import { PlayListHeaderComponent } from './components/play-list-header/play-list-header.component';
+import { PlayListBodyComponent } from './components/play-list-body/play-list-body.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +15,8 @@ import { SectionGenericComponent } from './components/section-generic/section-ge
     HeaderUserComponent,
     CardPlayerComponent,
     SectionGenericComponent,
+    PlayListHeaderComponent,
+    PlayListBodyComponent,
   ],
   imports: [CommonModule],
   exports: [
