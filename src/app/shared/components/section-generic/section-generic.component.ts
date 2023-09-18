926e418d6a0bb8e63e16b99af3060a4c5bd69467
Paul Llanque <llanquepol@gmail.com>
2023-09-18 14:30:40 -0500

    [FIX] section generic fixed

--- src/app/shared/components/section-generic/section-generic.component.ts
+++ src/app/shared/components/section-generic/section-generic.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.model';
 
 @Component({
   selector: 'app-section-generic',
@@ -8,5 +9,5 @@ import { Component, Input } from '@angular/core';
 export class SectionGenericComponent {
   @Input() title: string = '';
   @Input() mode: 'small' | 'big' = 'small';
-  @Input() dataTracks: any[] = [];
+  @Input() dataTracks: TrackModel[] = [];
 }
