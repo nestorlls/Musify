ba80c39affb46d5edda13c830a33f07912cf0576
Paul Llanque <llanquepol@gmail.com>
2023-09-18 16:04:53 -0500

    [ADD] track list in list body added

--- src/app/shared/components/play-list-body/play-list-body.component.ts
+++ src/app/shared/components/play-list-body/play-list-body.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.model';
+import * as dataRow from '../../../data/tracks.json';
 
 @Component({
   selector: 'app-play-list-body',
   templateUrl: './play-list-body.component.html',
-  styleUrls: ['./play-list-body.component.css']
+  styleUrls: ['./play-list-body.component.css'],
 })
 export class PlayListBodyComponent {
+  @Input() tracks: TrackModel[] = [];
+  optionSort: { property: string | null; order: string } = {
+    property: null,
+    order: 'asc',
+  };
 
+  constructor() {}
+
+  ngOnInit() {
+    const { data } = (dataRow as any).default;
+    this.tracks = data;
+  }
+  changeSort(property: string) {
+    const { order } = this.optionSort;
+    this.optionSort = {
+      property,
+      order: order === 'asc' ? 'desc' : 'asc',
+    };
+  }
 }
