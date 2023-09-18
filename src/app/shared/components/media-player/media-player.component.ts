23532109121e4d59f5f89e9c32139995b94db20f
Paul Llanque <llanquepol@gmail.com>
2023-09-18 11:24:35 -0500

    [ADD] TrackModel added

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.model';
 
 @Component({
   selector: 'app-media-player',
   templateUrl: './media-player.component.html',
-  styleUrls: ['./media-player.component.css']
+  styleUrls: ['./media-player.component.css'],
 })
 export class MediaPlayerComponent {
-
+  mockCover: TrackModel = {
+    cover: '',
+    album: '',
+    name: 'artist name',
+    url: '',
+    _id: '',
+  };
 }
